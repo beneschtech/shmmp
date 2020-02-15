8a51010858ab2ae652da5b31e2696e97abdd7f01
Chris Benesch <chris@beneschtech.com>
2020-02-15 14:35:36 -0700

    Initial commit

---
+++ include/shmmp.h
@@ -0,0 +1,7 @@
+#ifndef SHMPP_H
+#define SHMPP_H
+
+#include <config.h>
+#include <iostream>
+
+#endif // SHMPP_H
---
+++ src/shmmp.cpp
@@ -0,0 +1,7 @@
+#include <shmmp.h>
+
+int main(int,char**)
+{
+    std::cout << PACKAGE_NAME << ": " << PACKAGE_VERSION << std::endl;
+    return 0;
+}
